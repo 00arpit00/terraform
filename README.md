5220176699b57e5dc8370a32c9f3c41dcc45a5c8
Arpit Agarwal <arpitagarwal1308@gmail.com>
2025-10-04 15:42:08 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # terraform
 repository to manage terraform resources while preparing for certification
+
+This comment is from the host I am preparing from!!!
---
+++ ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 AMI (us-east-1)
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-example-first-instance-what-happens"
+  }
+}
